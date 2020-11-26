4b12886360b6c3197b04d6c023e4883921988010
loukseven <loukseven97@gmail.com>
2020-11-26 11:30:43 +0900

    Add my comp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import './App.css';
+import TitleMessage from './components/title-message/title-message.component'
 
 //components
 import MyNavbar from "./components/my-navbar/mynavbar.component";
+import MyCarousal from "./components/my-carousal/my-carousal.component";
 
 const App = () => {
   //두번째 테스트다테스트한다
   return (
     <div className="App">
       <MyNavbar />
-
+      <MyCarousal />
+      <TitleMessage />
     </div>
   );
 }
